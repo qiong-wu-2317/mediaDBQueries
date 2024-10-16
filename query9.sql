99164ae4607e744662afc27eeef9bfd1d17f00fe
qiong-wu-2317 <wuqiong2317@gmail.com>
2024-10-15 22:20:00 -0700

    fix answer to q9

--- query9.sql
+++ query9.sql
@@ -1,20 +1,17 @@
---query lists each customer's full name, their the total cost of purchases, time of first purchase and ranks them based on total cost and the time of their first purchase
+--the data of customer purchase history, which includes customer names, purchase time, track names purchased, and various spending statistics for each customer including each purchase cost, personal purchase rank, total cost, minimum cost, maximum Costs, and average cost.
 SELECT 
-    (FirstName || " " || LastName) AS full_name, 
-    first_consumption_time, 
-    total_cost,
-    RANK() OVER (ORDER BY total_cost DESC) AS cost_ranking,
-    RANK() OVER (ORDER BY first_consumption_time) AS time_rank
-FROM (
-	SELECT 
-        c.CustomerId, 
-        c.FirstName, 
-        c.LastName, 
-        MIN(inv.InvoiceDate) AS first_consumption_time, 
-        SUM(ii.UnitPrice * ii.Quantity) AS total_cost
-    FROM customers AS c
-    LEFT JOIN invoices AS inv ON c.CustomerId = inv.CustomerId
-    LEFT JOIN invoice_items AS ii ON inv.InvoiceId = ii.InvoiceId
-    GROUP BY c.CustomerId
-) AS customer_invoice_cost
-ORDER BY LastName, FirstName;
+    (c.FirstName || " " || c.LastName) AS full_name, 
+    inv.InvoiceDate,
+    group_concat(t.Name, ', ') OVER (PARTITION BY c.CustomerId, ii.InvoiceId) AS tracks_purchased,
+    inv.Total as invoce_cost,
+    rank() OVER (PARTITION BY c.CustomerId ORDER BY inv.Total DESC) AS personal_cost_rank,
+    sum(inv.total) OVER (PARTITION BY c.CustomerId) AS total_cost,
+    min(inv.total) OVER (PARTITION BY c.CustomerId) AS min_cost,
+    max(inv.total) OVER (PARTITION BY c.CustomerId) AS max_cost,
+    round(avg(inv.total) OVER (PARTITION BY c.CustomerId), 2) AS avg_cost
+FROM customers AS c
+LEFT JOIN invoices AS inv ON c.CustomerId = inv.CustomerId
+LEFT JOIN invoice_items AS ii ON inv.InvoiceId = ii.InvoiceId
+LEFT JOIN tracks AS t ON ii.TrackId = t.TrackId
+GROUP BY c.CustomerId, inv.InvoiceId
+ORDER BY total_cost DESC, c.LastName, c.FirstName, personal_cost_rank
